3e96d70ccf6430a2ff1b90c94a8fdb910d767623
LASER-WOLF <casper.eide@gmail.com>
2016-04-14 02:23:45 +0200

    ...

--- .OnboardComputerSystem/lcd-boot.py
+++ .OnboardComputerSystem/lcd-boot.py
@@ -2,5 +2,5 @@
      
 import Adafruit_CharLCD as LCD
 lcd = LCD.Adafruit_CharLCD(25, 24, 23, 17, 27, 22, 20, 4)
-#lcd.clear()
+lcd.clear()
 lcd.message('  ----------------  \n  ONBOARD COMPUTER  \n       SYSTEM       \n  ----------------  ')
