bdaaa48eca0d55daa2e0138faaf98016846a1b8d
LASER-WOLF <casper.eide@gmail.com>
2016-04-24 14:54:00 +0200

    ...

--- Scripts/update-server.sh
+++ Scripts/update-server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-camera_file=$(cd ~/Camera; ls | sort -n | tail -n 1);
 echo '' && echo '' && echo '----------------------------------------' && echo '** RUNNING SERVER UPDATE **' && echo '----------------------------------------'
-echo '' && echo '' && echo 'updating databases' && rsync -u -r ~/Databases/ username@website.com:folder/db/ && echo '- DONE'
-echo '' && echo '' && echo 'updating webcam' && rsync -u ~/Camera/$camera_file username@website.com:folder/img/cam01.jpg && echo '- DONE'
+echo '' && echo '' && echo 'copying databases' && rsync -u -r ~/Databases/ username@website.com:folder/db/ && echo '- DONE'
+echo '' && echo '' && echo 'copying camera files' && rsync -u -r ~/Camera/ username@website.com:folder/img/if-camera/ && echo '- DONE'
 echo '' && echo '' && echo '----------------------------------------' && echo '** DONE **' && echo '----------------------------------------' && echo '' && echo ''
